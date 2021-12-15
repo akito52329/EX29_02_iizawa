9b35f34cfef3d02389aa3f81d9b1fb015b9ead1f
sakuseimoto@gmail.com <sakuseimoto@gmail.com>
2021-12-15 09:47:00 +0900

    プロジェクト ファイルを追加します。

---
+++ EX29Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EX29
+{
+    class EX29Main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"表面積は{Box.GetVolume(3.5f, 2.5f, 1.5f)}、面積は{Box.GetSurface(3.5f,2.5f,1.5f)}");
+        }
+    }
+}

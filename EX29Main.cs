b8f48bae9895dcf0f1066ce390bbf12d355ee0ec
sakuseimoto@gmail.com <sakuseimoto@gmail.com>
2021-12-15 09:48:05 +0900

    yes

--- EX29Main.cs
+++ EX29Main.cs
@@ -6,7 +6,7 @@ namespace EX29
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"表面積は{Box.GetVolume(3.5f, 2.5f, 1.5f)}、面積は{Box.GetSurface(3.5f,2.5f,1.5f)}");
+            Console.WriteLine($"表面積は{Box.GetVolume(3.5f, 2.5f, 1.5f)}、面積は{Box.GetSurface(3.5f, 2.5f, 1.5f)}");
         }
     }
 }
